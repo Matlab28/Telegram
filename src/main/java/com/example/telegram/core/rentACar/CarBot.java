f08920bf7e4296f5810ad6cda36baf147f717a06
Matlab Abbaszada <148482779+Matlab28@users.noreply.github.com>
2024-06-02 17:51:12 +0400

    Update CarBot.java

--- src/main/java/com/example/telegram/core/rentACar/CarBot.java
+++ src/main/java/com/example/telegram/core/rentACar/CarBot.java
@@ -12,12 +12,12 @@ public class CarBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "yourMusicSongs_bot";
+        return "YOUR_BOT_NAME";
     }
 
     @Override
     public String getBotToken() {
-        return "6627982719:AAHlTAMMWAJmUr-8rDe4N_z0whUsloFdiwk";
+        return "YOUR_BOT_TOKEN";
     }
 
     @Override
